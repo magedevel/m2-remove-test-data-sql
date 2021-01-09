a8ad51fe163b8c07e449edc4ddad847a68f849d5
Magedevel.com <20953761+magedevel@users.noreply.github.com>
2021-01-09 12:24:37 +0400

    Update remove-all.sql
    Add remove categories.

--- remove-all.sql
+++ remove-all.sql
@@ -162,6 +162,37 @@ TRUNCATE TABLE `report_viewed_product_aggregated_monthly`;
 TRUNCATE TABLE `report_viewed_product_aggregated_yearly`;
 TRUNCATE TABLE `report_viewed_product_index`;
 
+TRUNCATE TABLE catalog_category_entity;
+
+TRUNCATE TABLE catalog_category_entity_datetime;
+
+TRUNCATE TABLE catalog_category_entity_decimal;
+
+TRUNCATE TABLE catalog_category_entity_int;
+
+TRUNCATE TABLE catalog_category_entity_text;
+
+TRUNCATE TABLE catalog_category_entity_varchar;
+
+TRUNCATE TABLE catalog_category_product;
+
+TRUNCATE TABLE catalog_category_product_index;
+
+INSERT INTO `catalog_category_entity` (`entity_id`, `attribute_set_id`, `parent_id`, `created_at`, `updated_at`, `path`, `position`, `level`, `children_count`) VALUES ('1', '0', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', '0', '0', '1'),
+('2', '3', '1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1/2', '1', '1', '0');
+
+INSERT INTO `catalog_category_entity_int` (`value_id`, `attribute_id`, `store_id`, `entity_id`, `value`) VALUES 
+('1', '69', '0', '1', '1'),
+('2', '46', '0', '2', '1'),
+('3', '69', '0', '2', '1');
+
+INSERT INTO `catalog_category_entity_varchar` (`value_id`, `attribute_id`, `store_id`, `entity_id`, `value`) VALUES 
+('1', '45', '0', '1', 'Root Catalog'),
+('2', '45', '0', '2', 'Default Category');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+DELETE FROM url_rewrite WHERE entity_type = 'category';
 
 SET FOREIGN_KEY_CHECKS = 1;
 
